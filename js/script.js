16fd4bd1dcc547095cc6906cff1356db81afd492
Tejaskadam20 <tejaskadam7267@gmail.com>
2024-06-04 12:33:22 +0530

    Script code changes

--- js/script.js
+++ js/script.js
@@ -5,4 +5,23 @@ var typed= new Typed("typing",{
 	typeSpeed:100,
 	BackSpeed:60,
 	loop:true,
-})
+});
+
+
+
+<script>
+  document.getElementById('downloadCvBtn').addEventListener('click', function() {
+    // Set the href to the path of the CV file
+    const link = 'Desktop/Tejas Kadam CV.pdf';
+
+    // Open the link in a new window or tab
+    window.open(link);
+
+    // Log a message to the console (optional)
+    console.log('');
+  });
+
+
+const emailLink = document.getElementById("emailid");
+emailLink.addEventListener("click", redirecttogmail);
+</script>
